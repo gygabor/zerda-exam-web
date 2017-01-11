e75c80ad562fd6daa26b20af1bf846774d521d6d
Gyorgy Gabor <gyorgyg@gmail.com>
2017-01-11 11:29:40 +0100

    validator and test are done

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
 'use strict';
 
 var test = require('tape');
+var validator = require('./validator.js')
 
-test("validator accepts valid email, a scale of 15 and positive feedback");
-test("validator rejects unfilled email, a scale of 15 and positive feedback");
-test("validator rejects valid email, a scale of 9 and positive feedback");
+test("validator accepts valid email, a scale of 15 and positive feedback", function (t){
+  t.equal(validator.validation('super', '15', 'sdfsdf@fff.com'), true);
+  t.end();
+});
+
+
+test("validator rejects unfilled email, a scale of 15 and positive feedback", function (t){
+  t.equal(validator.validation('super', '15', 'sdfsdffff.com'), false);
+  t.end();
+});
+
+test("validator rejects valid email, a scale of 9 and positive feedback", function (t){
+  t.equal(validator.validation('super', '9', 'sdfsdf@fff.com'), false);
+  t.end();
+});
--- validator.js
+++ validator.js
@@ -2,9 +2,31 @@
 
 
 var validator = (function(){
+  var json = require('./words.json');
+  var words = json[0].words;
 
   function validation (feedback, scale, email){
-    return (feedback, scale, email);
+    if ((email.indexOf('@') != -1) && (scale >= parseInt(10)) && feedback){
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  function findWord(feedback){
+    var feedArray = feedback.split(' ');
+    var findNumber = 0;
+    words.forEach(function(word){
+      var find = feedArray.indexOf(word)
+      if (find != -1){
+        findNumber++;
+      }
+    })
+    if (findNumber >= 3){
+      return true;
+    } else {
+      return false;
+    }
   }
 
   return {
